5d436affc0a097363da806d6864b5460fb4a0fe3
Oksana Kormilina <keysb@mail.ru>
2024-01-14 15:42:21 +0400

    Add timer functional

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
+
+
 export default function timerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  let timer
+
+  function handleStart() {
+   timer =  setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+clearTimeout(timer)
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-      <button>
-        Start Challenge
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
         </button>
-       </p> 
-      <p className="">
-        Time is running... / Timer inactive
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'} 
       </p>
     </section>
   );
