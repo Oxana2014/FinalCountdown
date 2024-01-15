97d13346cb9819f3b8d728b90bf03a0de9dca625
Oksana Kormilina <keysb@mail.ru>
2024-01-15 16:48:18 +0400

    Add score and reset in esc closing

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -7,6 +7,7 @@ const ResultModal = forwardRef(function ResultModal(
 
     const userLost = timeRemaining <= 0
     const formattedRemainingTime = (timeRemaining / 1000).toFixed(2)
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000))*100)
 
   const dialog = useRef();
   useImperativeHandle(ref, () => {
@@ -18,8 +19,9 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
      {userLost && <h2>You lost</h2>} 
+     {!userLost && <h2>Your score: {score}</h2>}
       <p>The target time was {targetTime} seconds</p>
       <p>
         You stopped with <strong>{formattedRemainingTime} seconds left</strong>
