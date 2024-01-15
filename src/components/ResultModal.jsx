883f2065d16e1f2c0db7c0a767f3e0093550f265
Oksana Kormilina <keysb@mail.ru>
2024-01-15 16:00:56 +0400

    Create exposing API in ResultModal component

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,20 @@
-import {forwardRef} from 'react'
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog ref={ref} className="result-modal" >
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>The target time was {targetTime} seconds</p>
       <p>
@@ -13,6 +25,6 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
       </form>
     </dialog>
   );
-})
+});
 
-export default ResultModal
+export default ResultModal;
